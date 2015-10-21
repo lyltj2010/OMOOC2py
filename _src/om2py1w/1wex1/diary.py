06f7bed273c8fe0a52fb44578c5eb2288842655a
9-win-yong <lyuyongle@gmail.com>
2015-10-21 00:12:43 -0500

    Update diary.py

--- _src/om2py1w/1wex1/diary.py
+++ _src/om2py1w/1wex1/diary.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-import sys
 
 fr = open('diary.txt','r')
 for line in fr:
 	print line,
+print
 fr.close()
 
-script, contents = sys.argv
+contents = raw_input("Anything want to share?\n")
 fw = open('diary.txt','a+')
 fw.write(contents+'\n')
 fw.close()
