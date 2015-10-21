b285ed556ea983ea22715eafe6d9dbdceb191746
9-win-yong <lyuyongle@gmail.com>
2015-10-21 00:17:08 -0500

    Update diary.py

--- _src/om2py1w/1wex1/diary.py
+++ _src/om2py1w/1wex1/diary.py
@@ -9,4 +9,15 @@ fr.close()
 contents = raw_input("Anything want to share?\n")
 fw = open('diary.txt','a+')
 fw.write(contents+'\n')
+
+while True:
+	yn = raw_input('Do you wanna share more?(y/n)')
+	if yn == 'n':
+		break
+	elif yn == 'y':
+		contents = raw_input("Okay, let's share more!\n")
+		fw.write(contents+'\n')
+	else:
+		print "Please type in 'y' or 'n'."
+
 fw.close()
