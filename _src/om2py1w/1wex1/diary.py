9cc58cbe2984dac8fc90e7a35b58c8f013dfc835
yongle <lyuyongle@gmail.com>
2015-10-25 14:58:57 -0500

    用函数实现各个功能，思路很清晰。

--- _src/om2py1w/1wex1/diary.py
+++ _src/om2py1w/1wex1/diary.py
@@ -1,23 +1,38 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
-fr = open('diary.txt','r')
-for line in fr:
-	print line,
-print
-fr.close()
 
-contents = raw_input("Anything want to share?\n")
-fw = open('diary.txt','a+')
-fw.write(contents+'\n')
+def ReadDiary():
+	f = open('MyDiary.txt','r')
+	print 5*'='+'Diary begins'+5*'='
 
-while True:
-	yn = raw_input('Do you wanna share more?(y/n)')
-	if yn == 'n':
-		break
-	elif yn == 'y':
-		contents = raw_input("Okay, let's share more!\n")
-		fw.write(contents+'\n')
-	else:
-		print "Please type in 'y' or 'n'."
+	for line in f:
+		print line,
+	print 6*'='+'Diary ends'+6*'='+'\n'
 
-fw.close()
+	f.close()
+
+def WriteDiary():
+	print "Anything want to share?"
+	f = open('MyDiary.txt','a+')
+
+	while True:
+		contents = raw_input(">>")
+
+		if contents.upper() in ['?','HELP']:
+			# use \ 换行
+			helpdoc = "Press ENTER to continue writing.\n"\
+			+"Enter EXIT, QUIT or Q to quit.\n"\
+			+"Enter HELP or '?' for help" 
+			print helpdoc
+		
+		if contents.upper() in ['Q','QUIT','EXIT']:
+			# str.lower() or str.upper() to convert
+			break
+		f.write(contents+'\n')
+	
+	f.close()
+
+#把独立的功能放在一个个函数里，思路清晰了不少。
+ReadDiary()
+WriteDiary()
