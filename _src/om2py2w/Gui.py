9e8ea0f290e467defdd9e37775fe24cf9082dcee
yongle <lyuyongle@gmail.com>
2015-10-29 00:13:28 -0500

    修改，用class创建GUI

--- _src/om2py2w/Gui.py
+++ _src/om2py2w/Gui.py
@@ -1,43 +1,45 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
-from Tkinter import *
-root = Tk()
-root.title('少年，吐槽时间到了！')
+import Tkinter as tk
 
-top_frame = Frame(root)
-top_frame.pack()
-middle_frame = Frame(root)
-middle_frame.pack()
-bottom_frame = Frame(root)
-bottom_frame.pack(side=BOTTOM)
+class Windows():
 
-mood_label = Label(top_frame,text='心情:')
-a = Checkbutton(top_frame,text='好')
-b = Checkbutton(top_frame,text='中')
-c = Checkbutton(top_frame,text='差')
-mood_label.grid(row=0,column=0)
-a.grid(row=0,column=1)
-b.grid(row=0,column=2)
-c.grid(row=0,column=3)
-blank_label = Label(top_frame,text=40*' ')
-blank_label.grid(row=0,column=4)
-weather_label = Label(top_frame,text='天气:')
-a_1 = Checkbutton(top_frame,text='晴')
-b_1 = Checkbutton(top_frame,text='阴天')
-c_1 = Checkbutton(top_frame,text='雨雪')
-weather_label.grid(row=0,column=5)
-a_1.grid(row=0,column=6)
-b_1.grid(row=0,column=7)
-c_1.grid(row=0,column=8)
+	def __init__(self,master):
+		self.master = master
+		self.master.title('少年，吐槽时间到了！')
 
-name_label = Label(middle_frame,text='日记名称')
-name_entry = Entry(middle_frame)
-name_label.grid(row=0,column=0,sticky=E)
-name_entry.grid(row=0,column=1)
+		self.top_frame = tk.Frame(self.master)
+		self.name_label = tk.Label(self.top_frame,text='日记名称')
+		self.name_entry = tk.Entry(self.top_frame)
+		self.top_frame.pack()
+		self.name_label.grid(row=0,column=0)
+		self.name_entry.grid(row=0,column=1)
 
-content_label = Label(bottom_frame,text="内容")
-content_text = Text(bottom_frame)
-content_label.grid(row=0,column=0)
-content_text.grid(row=0,column=1)
 
-root.mainloop()
+		self.middle_frame = tk.Frame(self.master)
+		self.content_label = tk.Label(self.middle_frame,text="内容")
+		self.content_text = tk.Text(self.middle_frame)
+		self.middle_frame.pack()
+		self.content_label.grid(row=0,column=0)
+		self.content_text.grid(row=0,column=1)
+
+		self.bottom_frame = tk.Frame(self.master)
+		self.menu_label_1 = tk.Button(self.bottom_frame,text="Save",bg='grey')
+		self.menu_label_2 = tk.Button(self.bottom_frame,text="Discard",bg='red')
+		self.menu_label_3 = tk.Button(self.bottom_frame,text="About",bg='grey')
+		self.bottom_frame.pack()
+		self.menu_label_1.grid(row=0,column=0)
+		self.menu_label_2.grid(row=0,column=1)
+		self.menu_label_3.grid(row=0,column=2)
+
+	def foo():
+		pass
+		
+
+def main():
+	root = tk.Tk()
+	app = Windows(root)
+	root.mainloop()
+
+if __name__ == "__main__":
+	main()
