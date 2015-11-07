d4236c6c90fa5f0c63bb33cc208e59981874a5b9
yongle <lyuyongle@gmail.com>
2015-11-07 12:56:38 -0600

    sendto, recvfrom

--- _src/om2py3w/client.py
+++ _src/om2py3w/client.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
-from socket import *
+from socket import * #for socket
+import sys #for exit
 
-s = socket(AF_INET,SOCK_DGRAM)
+try:
+	s = socket(AF_INET,SOCK_DGRAM)
+except socket.error:
+	print 'Failed to create socket.'
+	sys.exit()
+
 host = gethostbyname(gethostname())
 port = 8888
 server_addr = (host,port)
 
-text = 'history'
-s.sendto(text,server_addr)
+while True:
+	msg = raw_input('Enter message to send: ')
+	#send the msg.
+	s.sendto(msg,server_addr)
+
+	if msg == 'quit':
+		break
+	#receive data from client
+	reply, addr = s.recvfrom(1024)
+	print 'Server reply:\n'+ reply
