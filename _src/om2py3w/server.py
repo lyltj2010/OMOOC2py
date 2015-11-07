8ccf7bbee85c464884b189540bae532422e46512
yongle <lyuyongle@gmail.com>
2015-11-07 12:57:52 -0600

    recvfrom, sendto(reply,addr(not server addr,lad!))

--- _src/om2py3w/server.py
+++ _src/om2py3w/server.py
@@ -8,35 +8,46 @@ def history():
 	f.close()
 	return diary
 
-def write():
-	content = raw_input("Any thing want to share?\n>>")
+def write(data):
 	f = open('diary.txt','a+')
-	f.write(content)
+	f.write(data+'\n')
 	f.close()
 
 def help():
-	help_doc = "Keyin 'history' to show past diary,\
-	'quit' to quit, 'help' for help document and \
-	anything else to enter writing mode."
-	print help_doc
+	help_doc = ['Key in: ','"history" to diaplay past diary',\
+	'"help" to get guide','"" to write diary','"quit" to quit'\
+	'or type your diary directly.\n']
 
-s = socket(AF_INET,SOCK_DGRAM)
-host = gethostbyname(gethostname())
-port = 8888
-server_addr = (host,port)
-s.bind(server_addr)
+	help_doc = '\n'.join(help_doc)
+	return help_doc
 
-while True:
-	print "Waiting for connection!"
-	data, addr = s.recvfrom(1024)
-	if data == 'history':
-		print history()
-	elif data == 'help':
-		help()
-	elif data == 'quit':
-		break
-	else:
-		write()
+def main():
+	s = socket(AF_INET,SOCK_DGRAM)
+	host = gethostbyname(gethostname())
+	port = 8888
+	server_addr = (host,port)
+	s.bind(server_addr)
 
-s.close()
+	while True:
+		print "Waiting for connection!"
+		data, addr = s.recvfrom(1024)
+		print "Get connected by: %s" % str(addr)
 
+		if data == 'history':
+			reply = history()
+			s.sendto(reply,addr)
+		elif data == 'help':
+			reply = help()
+			s.sendto(reply,addr)
+		elif data == 'quit':
+			break
+		else:
+			write(data)
+			reply = "Diary already saved.\n"+\
+			"This is what you have written:\n"+data
+			s.sendto(reply,addr)
+
+	s.close()
+
+if __name__ == "__main__":
+	main()
